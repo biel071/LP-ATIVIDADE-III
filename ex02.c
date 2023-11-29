225a8b7128f4dafb2a22b937a1f82e0806aa5ef1
biel071 <144127906+biel071@users.noreply.github.com>
2023-11-29 15:45:57 -0300

    ex02.c

--- ex02.c
+++ ex02.c
@@ -1,50 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <ctype.h>
+#include <locale.h>
 
-int main() {
-int i, j; 
-char nome [3] [200];
-int idade [3];
-float notas [3];
-int somanotas=0;
-float media [3];
+int main(){
 
+    setlocale(LC_ALL,"portuguese");
 
-
-for ( i = 0; i < 3; i++)
-{
-    printf("Digite seu nome:");
-    gets (nome[i]);
-
-    printf ("Digite sua idade:");
-    scanf("%d",&idade[i]);
-for ( j = 0; j < 3; j++)
-{
-
-    printf("Digite suas notas:");
-    scanf("%.1f",&notas[j]);
-     fflush (stdin);
-}
-
-   somanotas+= notas[j];
-   media[j] = somanotas / 3;
-}
-for ( i = 0; i < 3; i++)
-{
-printf ("Seu nome:%s\n", nome[i]);
-printf ("Sua idade:%d\n",idade[i]);
-printf ("Sua media:%.1f\n", media[j]);
-    
-}
+    int i,j, idade[5];
+    char nome[5][200];
+    float nota[5][3],media[3]={0,0,0};
 
 
 
+    for (i=0;i<5;i++){
+
+       
+        printf(" Informe o nome : ");
+        gets(nome[i]);
+        
+        printf(" Informe a idade: ");
+        scanf("%i",&idade[i]);
+
+        printf(" Notas:\n");
+        for(j=0;j<3;j++){
+            printf("  %iª Nota:",j+1);
+            scanf("%f",&nota[i][j]);
+            media[i]+=nota[i][j]/3;
+        }
+        printf("\n");
+
+        fflush(stdin);
+    }
+
+    system("cls || clear");
 
 
+  
 
+    for(i=0;i<5;i++){
+        printf("NOME : %s\n",nome[i]);
+        printf("IDADE: %i\n",idade[i]);
+        printf("NOTAS\n");
+        for(j=0;j<3;j++){
+            printf("%iº TRIMESTRE: %.2f\n",j+1,nota[i][j]);
+        }
+        printf("MÉDIA: %.2f\n",media[i]);
+        printf("RESULTADO: ");
 
+        if(media[i]>=7){
+            printf("APROVADO\n");
+        }else if(media[i]>=5){
+            printf("RECUPERAÇÃO\n");
+        }else{
+            printf("REPROVADO\n");
+        }
+        printf("\n");
 
+    }
 
     return 0;
 }
